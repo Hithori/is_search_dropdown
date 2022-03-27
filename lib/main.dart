c85452cb596184f1a2bdfe88c3ed9a16aeb5e47d
Hitori (mac) <HitoriSun@gmail.com>
2022-03-27 21:03:10 +0300

    feat: add prototype dropdown

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rect_getter/rect_getter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +11,151 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const CupertinoApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            ISDropDown(),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class ISDropDown extends StatefulWidget {
+  const ISDropDown({Key? key}) : super(key: key);
+
+  @override
+  State<ISDropDown> createState() => _ISDropDownState();
+}
+
+class _ISDropDownState extends State<ISDropDown> {
+  final GlobalKey<RectGetterState> itemKey = RectGetter.createGlobalKey();
+  GlobalKey inputKey = GlobalKey();
+  late OverlayEntry _overlayEntry;
+  late bool isOpen = false;
+  String _text = '';
+
+  void openDropdown() {
+    isOpen = true;
+    // if (widget.onOpen != null) {
+    //   widget.onOpen!(isOpen);
+    // }
+    _overlayEntry = _createOverlayEntry();
+    Overlay.of(inputKey.currentContext!)!.insert(_overlayEntry);
+  }
+
+  void closeDropdown() {
+    isOpen = false;
+    // if (widget.onOpen != null) {
+    //   widget.onOpen!(isOpen);
+    // }
+    _overlayEntry.remove();
+  }
+
+  OverlayEntry _createOverlayEntry() {
+    return OverlayEntry(
+      builder: (context) => Stack(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: closeDropdown,
+            ),
+          ),
+          Positioned.fromRect(
+            rect: Rect.fromCenter(
+              center: RectGetter.getRectFromKey(itemKey)!.bottomCenter.translate(0, 100),
+              width: MediaQuery.of(context).size.width - 32,
+              height: 200,
+            ),
+            child: Material(
+              child: Container(
+                color: CupertinoColors.extraLightBackgroundGray,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ...List.generate(
+                        15,
+                        (index) => SizedBox(
+                          width: double.infinity,
+                          child: CupertinoButton(
+                            color: CupertinoColors.activeOrange,
+                            child: Text('${index.toString() != _text ? index : 'этот'}'),
+                            onPressed: () async {
+                              log('$index', name: 'onPressed dropDown-element');
+                              Future.delayed(
+                                const Duration(milliseconds: 250),
+                                () => closeDropdown(),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return RectGetter(
+      key: itemKey,
+      child: WillPopScope(
+        onWillPop: () async {
+          if (isOpen) {
+            // await dropdownBodyChild.currentState!.animationReverse();
+            closeDropdown();
+            return Future.value(false);
+          } else {
+            return Future.value(true);
+          }
+        },
+        child: CupertinoTextField(
+          key: inputKey,
+          onTap: () {
+            openDropdown();
+          },
+          onChanged: (text) {
+            setState(() {
+              _text = text;
+            });
+            log(_text, name: 'textField onChange');
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
